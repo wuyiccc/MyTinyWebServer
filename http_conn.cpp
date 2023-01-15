0500efa8411c65b516479c8a9529560c9a265c3b
wuyiccc <3406324191@qq.com>
2023-01-15 21:44:17 +0800

    finished read write func

--- http_conn.cpp
+++ http_conn.cpp
@@ -50,6 +50,121 @@ void modfd(int epollfd, int fd, int ev) {
 int http_conn::m_user_count = 0;
 int http_conn::m_epollfd = -1;
 
+// close http connection
+void http_conn::close_conn() {
+    if (m_sockfd != -1) {
+        removefd(m_epollfd, m_sockfd);
+        m_sockfd = -1;
+        m_user_count--;
+    }
+}
+
+
+// init connection
+void http_conn::init(int sockfd, const sockaddr_in &addr) {
+    m_sockfd = sockfd;
+    m_address = addr;
+
+    // reuse port
+    int reuse = 1;
+    setsockopt(m_sockfd, SOL_SOCKET, SO_REUSEPORT, *reuse, sizeof(reuse));
+    addfd(m_epollfd, sockfd, true);
+    m_user_count++;
+    init();
+}
+
+void http_conn::init() {
+    bytes_to_send = 0;
+    bytes_have_send = 0;
+
+    m_check_state = CHECK_STATE_REQUESTLINE;    // 初始状态为检查请求行
+    m_linger = false;       // 默认不保持链接  Connection : keep-alive保持连接
+
+    m_method = GET;         // 默认请求方式为GET
+    m_url = 0;
+    m_version = 0;
+    m_content_length = 0;
+    m_host = 0;
+    m_start_line = 0;
+    m_checked_idx = 0;
+    m_read_idx = 0;
+    m_write_idx = 0;
+
+    bzero(m_read_buf, READ_BUFFER_SIZE);
+    bzero(m_write_buf, READ_BUFFER_SIZE);
+    bzero(m_real_file, FILENAME_LEN);
+}
+
+bool http_conn::read() {
+    if (m_read_idx >= READ_BUFFER_SIZE) {
+        return false;
+    }
+
+    int bytes_read = 0;
+    while (true) {
+        bytes_read = recv(m_sockfd, m_read_buf + m_read_idx, READ_BUFFER_SIZE - m_read_idx, 0);
+        if (bytes_read == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                break;
+            }
+            return false;
+        } else if (bytes_read == 0) {
+            return false;
+        }
+        m_read_idx += bytes_read;
+    }
+    return true;
+}
+
+bool http_conn::write() {
+    int temp = 0;
+
+    if (bytes_to_send == 0) {
+        modfd(m_epollfd, m_sockfd, EPOLLIN);
+        init();
+        return true;
+    }
+
+    while (1) {
+        temp = writev(m_sockfd, m_iv, m_iv_count);
+        if (temp <= -1) {
+            // if tcp hasn't buffer, wait next epollout event
+            if (errno == EAGAIN) {
+                modfd(m_epollfd, m_sockfd, EPOLLOUT);
+                return true;
+            }
+            unmap();
+            return false;
+        }
+        bytes_have_send += temp;
+        bytes_to_send -= temp;
+
+        if (bytes_have_send >= m_iv[0].iov_len) {
+            m_iv[0].iov_len = 0;
+            m_iv[1].iov_base = m_file_address + (bytes_have_send - m_write_idx);
+            m_iv[1].iov_len = bytes_to_send;
+        } else {
+            m_iv[0].iov_base = m_write_buf + bytes_to_send;
+            m_iv[0].iov_len = m_iv[0].iov_len - temp;
+        }
+
+        if (bytes_to_send <= 0) {
+            unmap();
+            modfd(m_epollfd, m_sockfd, EPOLLIN);
+            if (m_linger) {
+                init();
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+}
+
+
+
+
 
 
 
